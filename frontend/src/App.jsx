5b79f62ebd2093afcbeb58eb4c0b059e30bd74b4
Yasithb <150070850+Yasithb@users.noreply.github.com>
2024-08-08 16:22:56 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 
 
 const App = () => {
   return (
     <div className='app'>
       <Navbar/>
+      <Routes>
+        <Route path = '/' element = {<Home/>} />
+        <Route path = '/cart' element = {<Cart/>} />
+        <Route path = '/oder' element = {<PlaceOrder/>} />
+
+      </Routes>
       <img src=''/>
     </div>
   )
