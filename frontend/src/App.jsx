5b5d360ca68f20e35f0d3cebd38efd498ae297a9
Yasithb <150070850+Yasithb@users.noreply.github.com>
2024-08-08 18:43:16 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,22 @@
-import React from 'react'
-import Navbar from './components/Navbar/Navbar'
-import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
-
+import React from 'react';
+import Navbar from './components/Navbar/Navbar';
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Cart from './pages/Cart/Cart';
 
 const App = () => {
   return (
     <div className='app'>
-      <Navbar/>
+      <Navbar />
       <Routes>
-        <Route path = '/' element = {<Home/>} />
-        <Route path = '/cart' element = {<Cart/>} />
-        <Route path = '/oder' element = {<PlaceOrder/>} />
-
+        <Route path='/' element={<Home />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/order' element={<PlaceOrder />} />
       </Routes>
-      <img src=''/>
+      
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
